80da4b571bb78eb6666c61be7d4e260614e2c4a7
DELL <DELL@DESKTOP-VBTLK2K>
2024-04-11 21:16:55 +0000

    Add project files.

---
+++ EmployeeCRUDBlazor/Context/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EmployeeCRUDBlazor.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeCRUDBlazor.Context
+{
+    public class ApplicationDbContext:DbContext
+    {
+       
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Employee>Employees { get; set; }
+    }
+}
